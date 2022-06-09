38a27bcc46ca12f384961bb17a7f915c5b75d3d9
Krypto-Kazz <kryptokazz@tutanota.com>
2022-06-09 21:34:37 +1000

    weekdays

--- app.js
+++ app.js
@@ -5,18 +5,29 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req,res){
+app.set("view engine", "ejs");
 
-    let today = new Date();
-    let currentDay = today.getDay();
+app.get("/", function (req, res) {
 
-    if (currentDay == 6 || currentDay == 0) {
-        res.send("Yay it's the weekend");
-    } else {
-        res.send("Boo! I have to work!");
-    }
+  let today = new Date();
+
+  let options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
+  };
+
+
+  let day = today.toLocaleDateString("en-US",options);
+
+
+
+  res.render("list", {
+      kindOfDay: day
+    });
+
 });
 
-app.listen(3000,function(){
-    console.log("Server started on port 3000");
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
